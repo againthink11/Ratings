1ac51fc1b1deafbca30aa33b68d6c187ca2b37bd
zeeshan7668 <muhammadz43753@gmail.com>
2024-01-05 12:18:20 +0500

    ratings added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Ratings maxRating={10} color='orange' size={24} setRates={setRates}/>
-      {/* <Ratings maxRating={5} color='orange' size={24} message={['Terrible', 'Bad', 'Okey', 'Good', 'Amazing']} setRates={setRates}/> */}
+      <Ratings maxRating={5} color='orange' size={24} message={['Terrible', 'Bad', 'Okey', 'Good', 'Amazing']} setRates={setRates}/>
       <p>This Movie is rated {rates} stars.</p>
     </div>
   );
